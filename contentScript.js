ed99e72879026605186192b956515225fe0a505f
Andres Bonilla <andresbonilla1256@gmail.com>
2023-08-14 23:29:44 -0500

    primer avance de la extension

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,22 @@
 (() => {
-  
+  console.log("Sadasdasd")
+  const url = 'https://rickandmortyapi.com/api/character';
+
+  // Realizar la solicitud usando la Fetch API
+  fetch(url)
+    .then(response => {
+      if (response) {
+        return response.json();
+      } else {
+        console.error("error")
+        return;
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 })();
 
