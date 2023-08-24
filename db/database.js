7ac3dadc44189d57a985af2e85f17a4b5f35a89f
A-N-D-R-E-S-B-O-N-I-L-L-A <andresbonilla1256@gmail.com>
2023-08-24 08:49:11 -0500

    temporal database info

---
+++ db/database.js
@@ -0,0 +1,194 @@
+export const summariePolicyList = [
+    {
+        name: 'facebook',
+        policyWebpage: 'https://es-la.facebook.com/legal/terms/',
+        privacyTerms: [
+          {
+            subtitle: "Varied Data Collection",
+            text: "Information gathered depends on product use (e.g., selling, posting), affecting the type of data collected."
+          },
+          {
+            subtitle: "Essential vs. Optional",
+            text: "Some data is required for product use, while optional data impacts user experience quality."
+          },
+          {
+            subtitle: "De-identified Usage",
+            text: "Certain info is de-identified for use without re-identifying individuals."
+          },
+          {
+            subtitle: "User Control",
+            text: "Users have control over their data, including access and review."
+          },
+          {
+            subtitle: "Personalization and Ads",
+            text: "Automated processing creates personalized experiences, excluding ad personalization from user-provided data."
+          },
+          {
+            subtitle: "Safety and Protection",
+            text: "Data ensures user safety, product security, and content effectiveness."
+          },
+          {
+            subtitle: "Inter-Company Data Sharing",
+            text: "Meta entities share data for security, personalization, innovation, and more."
+          },
+          {
+            subtitle: "User Rights and Privacy Management",
+            text: "Tools enable data management, rights exercise, and compliance with applicable laws."
+          }
+        ],
+        vigencyOfPrivacy: '15/06/2023',
+        conditionsTerms: [
+            {
+              subtitle: "Personalized Experience",
+              text: "Facebook offers a personalized experience, showing content based on user preferences and interactions. This covers everything from posts, stories, events, and ads on the news feed to pages followed and settings chosen. User data, interactions, and preferences contribute to this customization."
+            },
+            {
+              subtitle: "Connecting People",
+              text: "The platform helps users discover and connect with people, groups, businesses, and organizations of interest. Suggestions for joining groups, attending events, and following pages are based on user data and connections to create stronger and more meaningful communities."
+            },
+            {
+              subtitle: "Self-expression",
+              text: "Facebook provides multiple ways for users to express themselves and communicate with others, such as sharing updates, photos, videos, and stories. The platform also explores new technologies like augmented reality and 360° videos for more engaging content."
+            },
+            {
+              subtitle: "Content Discovery",
+              text: "Users are shown personalized ads, offers, and sponsored content to discover products and services offered by various businesses and organizations. Advertisers can target specific demographics and interests, and advertisers receive feedback on ad performance."
+            },
+            {
+              subtitle: "Safety",
+              text: "Ensuring user safety is a priority. Advanced technologies and global teams are employed to detect and counter inappropriate behavior. Meta shares data across its entities to maintain safety, assist users, and protect the community. Responses to harmful content include warnings, content removal, restricted access, and involvement of law enforcement."
+            },
+            {
+              subtitle: "Ad Payment",
+              text: "Instead of user fees, the platform's services are funded through ads and sponsored content. This enables free access to users while allowing businesses to promote their offerings."
+            },
+            {
+              subtitle: "User Privacy",
+              text: "While delivering personalized ads, Facebook doesn't reveal user identity to advertisers. Advertisers define target demographics and interests, and reports on ad performance are provided, safeguarding user privacy."
+            },
+            {
+              subtitle: "User Responsibilities",
+              text: "Users are expected to provide accurate information, follow content guidelines, and adhere to policies. By uploading content, users grant Facebook a license to use, distribute, modify, and create derivative works, contributing to the platform's functionality and services."
+            }
+        ],
+        vigencyofUseTerms: '26/07/22',
+    },
+    {
+      name: 'google',
+      policyWebpage: '',
+      privacyTerms: ``,
+      vigencyOfPrivacy: '',
+      conditionsTerms: ``,
+      vigencyofUseTerms: '',
+    },
+    {
+      name: 'tiktok',
+      policyWebpage: 'https://www.tiktok.com/legal',
+      privacyTerms: [
+        {
+          "subtitle": "Data Collection:",
+          "text": "Diverse user info, including profiles, content, messages, purchases, and identity proofs, is collected."
+        },
+        {
+          "subtitle": "Automated Tracking:",
+          "text": "User interactions, inferred traits, device details, location, and media attributes are gathered."
+        },
+        {
+          "subtitle": "Cookies and Tools:",
+          "text": "Cookies and tools aid data collection, analysis, and ad optimization."
+        },
+        {
+          "subtitle": "Third-Party Links:",
+          "text": "Third-party accounts and partners access data; advertisers and social media networks share user data."
+        },
+        {
+          "subtitle": "Data Usage:",
+          "text": "Collected data serves ads, personalization, security, analytics, and communication."
+        },
+        {
+          "subtitle": "Data Sharing:",
+          "text": "User data is shared with partners, advertisers, law enforcement, and affiliates. Public profiles and posts may be visible."
+        },
+        {
+          "subtitle": "Security Measures:",
+          "text": "User data is secured with technical and admin safeguards on global servers."
+        },
+        {
+          "subtitle": "User Control:",
+          "text": "Users have rights to access, modify, and manage data; can control cookies and privacy settings."
+        },
+        {
+          "subtitle": "Data Retention:",
+          "text": "Data is kept based on necessity for platform provision, legal compliance, and commercial interests."
+        },
+        {
+          "subtitle": "Child Protection:",
+          "text": "Platform not for users under 13; local laws may raise age requirement. Parent's Guide available."
+        },
+        {
+          "subtitle": "Policy Updates:",
+          "text": "Users informed of changes; continued use post-update implies acceptance."
+        },
+        {
+          "subtitle": "Contact:",
+          "text": "Users can address privacy concerns via provided link; complaints to data authorities also possible."
+        },
+        {
+          "subtitle": "Local Terms:",
+          "text": "Jurisdiction-specific terms apply; in Argentina, data protection rights exist."
+        },
+        {
+          "subtitle": "Secondary Uses:",
+          "text": "Data used for personalized ads, not critical for platform use; user can limit such use."
+        },
+        {
+          "subtitle": "Minor Consent:",
+          "text": "Parent's Consent section in Terms of Service provides guidance."
+        }
+      ],
+      vigencyOfPrivacy: '05/04/2023',
+      conditionsTerms: [
+        {
+          "subtitle": "Platform Usage",
+          "text": "TikTok, provided by TikTok Pte. Ltd., allows personal, non-commercial use, creating a user-TikTok agreement."
+        },
+        {
+          "subtitle": "Acceptance",
+          "text": "Accessing TikTok implies agreement to terms, Privacy Policy, and Community Guidelines, including jurisdiction-specific terms."
+        },
+        {
+          "subtitle": "Terms Changes",
+          "text": "TikTok can alter terms, with continued use signifying acceptance. Users should stay informed about changes."
+        },
+        {
+          "subtitle": "Account Responsibility",
+          "text": "Users must create accurate accounts, safeguard passwords, and report compromises; violations can lead to account suspension."
+        },
+        {
+          "subtitle": "Service Adherence",
+          "text": "Compliance with terms, laws, and Community Guidelines is essential. Unauthorized distribution and interference are restricted."
+        },
+        {
+          "subtitle": "Intellectual Property",
+          "text": "Respect for copyrights is vital; TikTok can take action against copyright infringement."
+        },
+        {
+          "subtitle": "Content Handling",
+          "text": "TikTok holds content ownership; users can generate value but lack revenue rights. User content is analyzed through automated systems."
+        },
+        {
+          "subtitle": "User Content",
+          "text": "User-generated content has crucial points:\n\nContent Variety: Users can share diverse content like photos, videos, and music.\nCollaboration: Users can create combined content, using TikTok's resources.\nResponsibility: Users ensure content compliance; TikTok doesn't verify content.\nConfidentiality: Users shouldn't upload confidential material.\nTikTok License: TikTok gains rights to use user content globally.\nPublic Access: Content might be public unless privacy settings are adjusted.\nModeration: TikTok can remove or block violating content; users advised to save personal copies.\nIndemnification: Users hold responsibility for their content; TikTok's indemnification right."
+        }
+      ],
+      vigencyofUseTerms: '00/02/2021',
+    },
+    {
+      name: 'product hunt',
+      policyWebpage: '',
+      privacyTerms: ``,
+      vigencyOfPrivacy: '',
+      conditionsTerms: ``,
+      vigencyofUseTerms: '',
+    },
+]
