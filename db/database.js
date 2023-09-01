828c65e8126161dd5417a9c616c30043447df2a7
A-N-D-R-E-S-B-O-N-I-L-L-A <andresbonilla1256@gmail.com>
2023-09-01 15:11:08 -0500

    pinterest info

--- db/database.js
+++ db/database.js
@@ -1812,11 +1812,77 @@ export const summariePolicyList = [
     },
     {
       name: 'pinterest',
-      policyWebpage: '',
-      privacyTerms: [],
-      vigencyOfPrivacy: '',
-      conditionsTerms: [],
-      vigencyofUseTerms: '',
+      policyWebpage: 'https://policy.pinterest.com/',
+      privacyTerms: [
+        {
+          subtitle: "Our Mission",
+          text: "Pinterest's mission is to inspire individuals to create their desired life by delivering personalized content and ads based on user data and preferences."
+        },
+        {
+          subtitle: "Scope of Privacy Policy",
+          text: "This is Pinterest's global privacy policy, with specific sections tailored to users based on their location. For users in the EEA, Switzerland, or the UK, Pinterest Europe Ltd. and Pinterest, Inc. share data controller responsibilities."
+        },
+        {
+          subtitle: "Information Collection",
+          text: "Pinterest collects data through various means, including user-provided information, interaction with content, location sharing, and customer support communication."
+        },
+        {
+          subtitle: "Technical Information",
+          text: "Automatically collected data includes device type, operating system, network provider, and unique identifiers."
+        },
+        {
+          subtitle: "Cookies and Similar Technologies",
+          text: "Pinterest uses cookies for purposes like language preferences and personalization, with more details available in the Cookie Policy."
+        },
+        {
+          subtitle: "Data from Partners",
+          text: "Pinterest receives data from affiliates, advertisers, technical service partners, and others to enhance user experiences and ad targeting."
+        },
+        {
+          subtitle: "How We Use Your Information",
+          text: "Pinterest employs user data for identification, content recommendations, query responses, community creation, location-based personalization, and communication."
+        },
+        {
+          subtitle: "Improving Services and Protecting Users",
+          text: "Pinterest reviews user activity to detect spam, suspicious behavior, or policy violations. Data analysis enhances products, generates aggregated or anonymized data, and ensures user safety and legal compliance."
+        }
+      ],
+      vigencyOfPrivacy: '01/08/23',
+      conditionsTerms: [
+        {
+          "subtitle": "Pinterest's Purpose",
+          "text": "Pinterest is all about inspiring users to create their desired life by curating content relevant to their interests. While advertisers may promote content, it will always be clearly marked."
+        },
+        {
+          "subtitle": "Usage and Age Limitations",
+          "text": "Users must follow the rules and adhere to applicable laws. Only individuals aged 13 or older (and meeting their country's minimum age requirement) are allowed to use Pinterest. Those aged 13 to 18 require parental or guardian consent. Users with deactivated accounts can't create new ones without explicit permission."
+        },
+        {
+          "subtitle": "Prohibited Actions",
+          "text": "Unauthorized access, collection, copying, or use of Pinterest's data or content is strictly prohibited. The service should not be used for unlawful activities and must comply with trade restrictions."
+        },
+        {
+          "subtitle": "User License",
+          "text": "Pinterest grants users a limited, non-exclusive, and revocable license to access and use the service, subject to these terms and policies. Commercial use requires a business account."
+        },
+        {
+          "subtitle": "User-Generated Content",
+          "text": "Users retain rights to the content they post, but it must adhere to the terms and policies. Pinterest can take action against content that violates these rules."
+        },
+        {
+          "subtitle": "Content Sharing and Licensing",
+          "text": "When users post content, they grant Pinterest and others a worldwide, non-exclusive license to use, store, and distribute it. Pinterest uses various factors to recommend content to users."
+        },
+        {
+          "subtitle": "Data Retention",
+          "text": "Pinterest may retain user content for backup or audit purposes even after account deletion. Other users may continue to use and share saved content."
+        },
+        {
+          "subtitle": "Security and Third-Party Links",
+          "text": "While Pinterest prioritizes security, users should safeguard their passwords. Pinterest does not take responsibility for third-party websites or services linked from its platform."
+        }
+      ],
+      vigencyofUseTerms: '01/08/23',
     },
     {
       name: 'espn',
