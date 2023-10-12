535ca0128f9ccc361974c672d966e21fe60ef2c4
Andres Bonilla <andresbonilla1256@gmail.com>
2023-10-11 21:23:49 -0500

    feedback header improved

--- popup.js
+++ popup.js
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", async() => {
       let summaryInfo = {
         privacy: [],
         terms: [],
-        token: ''
+        token: '',
+        host: ''
       };
+      let canGiveAlikeODislike = false;
+      let showRequestHeader = true;
 
       // not logged pages
       const notloggedPage = document.getElementById('not-logged');
@@ -46,11 +49,77 @@ document.addEventListener("DOMContentLoaded", async() => {
         termButton.className = ''
         verifyIfThereArePolicies(summaryInfo.privacy, 'privacy')
       });
-      
 
+      // like and dislike functionalities
+      const sendFeedBackUrl = "http://localhost:4200/api/summary";
+      const questionHeader = document.getElementById('question-header');
+
+      const likeButton = document.getElementById('like-button');
+      likeButton.addEventListener('click', async(e)=>{
+
+          e.preventDefault();
+          questionHeader.style.display = "none";
+          showRequestHeader = false;
+          if (canGiveAlikeODislike) {
+              try {
+                
+                const res =  await fetch(sendFeedBackUrl, {
+                                          method: 'POST',
+                                          headers: {
+                                              'Content-Type': 'application/json',
+                                              'x-token': summaryInfo.token
+                                          },
+                                          body: JSON.stringify({...summaryInfo,
+                                            policyWebpage: summaryInfo.host,
+                                            type: 'like'
+                                          })
+                                        }
+                                    )
+
+                const result = await res.json();                
+             
+              } catch (error) {
+                console.log(error);
+                questionHeader.style.display = "flex";
+                showRequestHeader = true;
+              }
+          }
+      });
+
+      const dislikeButton = document.getElementById('dislike-button');
+      dislikeButton.addEventListener('click', async(e)=>{
+
+          e.preventDefault();
+          questionHeader.style.display = "none";
+          showRequestHeader = false;
+          if (canGiveAlikeODislike) {
+              try {
+                
+                const res =  await fetch(sendFeedBackUrl, {
+                                          method: 'POST',
+                                          headers: {
+                                              'Content-Type': 'application/json',
+                                              'x-token': summaryInfo.token
+                                          },
+                                          body: JSON.stringify({...summaryInfo,
+                                            policyWebpage: summaryInfo.host,
+                                            type: 'dislike'
+                                          })
+                                        }
+                                    )
+
+                const result = await res.json();                
+             
+              } catch (error) {
+                console.log(error);
+                questionHeader.style.display = "flex";
+                showRequestHeader = true;
+              }
+          }
+      });
+      
+      //functions to set data
       const showSummaries = (list, type) => {
-        
-        let sendFeedBackUrl = (type === 'terms') ? "" : "";
 
         for (let i = 0; i < list.length; i++) {
 
@@ -59,66 +128,10 @@ document.addEventListener("DOMContentLoaded", async() => {
           h3.textContent = list[i].subtitle;
           const p = document.createElement('p');
           p.textContent = list[i].text;
+          li.appendChild(h3);
+          li.appendChild(p);
+          summaryHtmlText.appendChild(li);
           
-          if (i === 0) {
-
-            const questionDIV = document.createElement('div');
-            questionDIV.classList = 'question-header'
-
-            const questionText = document.createElement('p');
-            questionText.textContent = "Did you find this summary useful?";
-
-            const likeButton = document.createElement('button');
-            likeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/></svg>`;
-            likeButton.addEventListener('click', ()=>{
-              fetch(sendFeedBackUrl, {
-                                        method: 'POST',
-                                        headers: {
-                                            'Content-Type': 'application/json',
-                                            'x-token': summaryInfo.token
-                                        },
-                                        body: JSON.stringify({...summaryInfo,
-                                          policyWebpage: window.location.host,
-                                          type: 'like'
-                                        })
-                                      }
-              )
-            });
-      
-
-            const dislikeButton = document.createElement('button');
-            dislikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2H464c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48H294.5c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7V192v48 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384H96c17.7 0 32-14.3 32-32V128c0-17.7-14.3-32-32-32H32C14.3 96 0 110.3 0 128V352c0 17.7 14.3 32 32 32z"/></svg>`;
-            dislikeButton.addEventListener('click', ()=>{
-              fetch(sendFeedBackUrl, {
-                                        method: 'POST',
-                                        headers: {
-                                            'Content-Type': 'application/json',
-                                            'x-token': summaryInfo.token
-                                        },
-                                        body: JSON.stringify({...summaryInfo,
-                                          policyWebpage: window.location.host,
-                                          type: 'dislike'
-                                        })
-                                      }
-              )
-            });
-
-
-            questionDIV.appendChild(questionText);
-            questionDIV.appendChild(likeButton);
-            questionDIV.appendChild(dislikeButton);
-
-            li.appendChild(questionDIV);
-            li.appendChild(h3);
-            li.appendChild(p);
-            summaryHtmlText.appendChild(li);
-
-          }else{
-            li.appendChild(h3);
-            li.appendChild(p);
-            summaryHtmlText.appendChild(li);
-          }
-           
         }
       }
 
@@ -142,8 +155,14 @@ document.addEventListener("DOMContentLoaded", async() => {
 
       const verifyIfThereArePolicies = (terms, type) => {
         if (terms.length === 0) {
-          summaryHtmlText.textContent = `loading...`
+          summaryHtmlText.textContent = `loading...`;
+          canGiveAlikeODislike = false;
+          questionHeader.style.display = "none";
         }else{
+          canGiveAlikeODislike = true;
+          if (showRequestHeader) {
+            questionHeader.style.display = "flex";
+          }
           summaryHtmlText.innerHTML = '';
           showSummaries(terms, type);
         }
@@ -222,14 +241,10 @@ document.addEventListener("DOMContentLoaded", async() => {
               }
               
               if (request.serverData.ifPrivacy) {
-                  showSummaries(request.serverData.termsOfPrivacy)
                   policyList.textContent =  `${(request.serverData.ifPrivacy) ? 'Privacy Policy' : ''}, ${(request.serverData.ifTerms) ? 'Terms of Use' : ''}`
                   defaultText.style.display = 'none';
                   detectedText.style.display = 'block';
               }else{
-                  if (request.serverData.ifTerms) {
-                    showSummaries(request.serverData.termsOfUse);
-                  }
                   detectedText.style.display = 'none';
                   defaultText.style.display = 'block';
               }
@@ -238,7 +253,8 @@ document.addEventListener("DOMContentLoaded", async() => {
               summaryInfo = {
                 privacy: request.serverData.termsOfPrivacy,
                 terms: request.serverData.termsOfUse,
-                token: request.serverData.tokenValidator
+                token: request.serverData.tokenValidator,
+                host: request.serverData.host
               }
               
           }
